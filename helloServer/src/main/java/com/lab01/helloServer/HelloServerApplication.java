1f7ae37f4086c9cc2deda6f353b70c84c30b56e0
joaobb <joaopedro.bb98@gmail.com>
2019-05-22 17:45:01 -0300

    Adicao de backend

---
+++ helloServer/src/main/java/com/lab01/helloServer/HelloServerApplication.java
@@ -0,0 +1,13 @@
+package com.lab01.helloServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServerApplication.class, args);
+	}
+
+}
